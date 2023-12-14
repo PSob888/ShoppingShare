752b58a0a27612536ab146a734a004cb43246b65
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-12-14 16:21:10 +0100

    fix layout overflow

--- shopping_share/lib/screens/shopping_list_screen.dart
+++ shopping_share/lib/screens/shopping_list_screen.dart
@@ -5,9 +5,7 @@ import 'package:shopping_share/widgets/bottom_navbar.dart';
 import 'package:shopping_share/widgets/floating_buttons/floating_button.dart';
 import 'package:shopping_share/providers/MyAuthProvider.dart';
 import 'package:shopping_share/widgets/floating_buttons/floating_button_callbacks.dart';
-import 'package:shopping_share/screens/list_screen.dart';
 import 'package:intl/intl.dart';
-
 
 class ShoppingListsScreen extends StatefulWidget {
   const ShoppingListsScreen({Key? key}) : super(key: key);
@@ -23,46 +21,51 @@ class _ShoppingListsScreenState extends State<ShoppingListsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: const BottomBar(currentIndex: 0),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.grey,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        selectedButton = "my";
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: selectedButton == "my" ? Colors.blue : Colors.grey,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.grey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedButton = "my";
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary:
+                            selectedButton == "my" ? Colors.blue : Colors.grey,
+                      ),
+                      child: Text("My"),
                     ),
-                    child: Text("My"),
                   ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        selectedButton = "shared";
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: selectedButton == "shared" ? Colors.blue : Colors.grey,
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedButton = "shared";
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: selectedButton == "shared"
+                            ? Colors.blue
+                            : Colors.grey,
+                      ),
+                      child: Text("Shared"),
                     ),
-                    child: Text("Shared"),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ShoppingListsStream(),
-          ),
-        ],
+            Expanded(
+              child: ShoppingListsStream(),
+            ),
+          ],
+        ),
       ),
       backgroundColor: backgroundColor,
       floatingActionButton: CustomFAB(
@@ -78,7 +81,10 @@ class ShoppingListsStream extends StatelessWidget {
   Widget build(BuildContext context) {
     String? userId = _authProvider.user?.uid;
     return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('lists').where('user_id', isEqualTo: userId).snapshots(),
+      stream: FirebaseFirestore.instance
+          .collection('lists')
+          .where('user_id', isEqualTo: userId)
+          .snapshots(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
@@ -97,7 +103,8 @@ class ShoppingListsStream extends StatelessWidget {
 class ShoppingListsListView extends StatelessWidget {
   final AsyncSnapshot<QuerySnapshot> snapshot;
 
-  const ShoppingListsListView({Key? key, required this.snapshot}) : super(key: key);
+  const ShoppingListsListView({Key? key, required this.snapshot})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -107,7 +114,8 @@ class ShoppingListsListView extends StatelessWidget {
       itemCount: shoppingLists.length,
       itemBuilder: (context, index) {
         String listName = shoppingLists[index]['name'] ?? '';
-        Timestamp createdAtTimestamp = shoppingLists[index]['created_at'] ?? Timestamp.now();
+        Timestamp createdAtTimestamp =
+            shoppingLists[index]['created_at'] ?? Timestamp.now();
         DateTime createdAt = createdAtTimestamp.toDate();
         int itemAmount = shoppingLists[index]['itemAmount'] ?? 0;
         bool isDone = shoppingLists[index]['isDone'] ?? false;
@@ -117,12 +125,19 @@ class ShoppingListsListView extends StatelessWidget {
         return GestureDetector(
           onTap: () {
             // Navigate to the list screen
-            Navigator.pushNamed(context, '/list', arguments: {'parameter1': listName, 'parameter2': documentId, 'parameter3': amountSpend});
+            Navigator.pushNamed(context, '/list', arguments: {
+              'parameter1': listName,
+              'parameter2': documentId,
+              'parameter3': amountSpend
+            });
           },
           child: Dismissible(
             key: Key(documentId),
             onDismissed: (direction) {
-              FirebaseFirestore.instance.collection('lists').doc(documentId).delete();
+              FirebaseFirestore.instance
+                  .collection('lists')
+                  .doc(documentId)
+                  .delete();
             },
             background: Container(
               color: Color(0xFF8C2A35),
@@ -171,4 +186,3 @@ class ShoppingListsListView extends StatelessWidget {
     return DateFormat('yyyy-MM-dd HH:mm:ss').format(date);
   }
 }
-
