c596a42d09e98abf5751471e9d8e060a5f442c19
Marek Szulc <235991@edu.p.lodz.pl>
2023-12-14 22:05:59 +0100

    Add permissions prompt

--- shopping_share/lib/main.dart
+++ shopping_share/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'firebase_options.dart';
 
 import 'routes.dart';
@@ -13,6 +14,8 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+  Permission.notification.request();
+  Permission.location.request();
 }
 
 class MyApp extends StatelessWidget {
